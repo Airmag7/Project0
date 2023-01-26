97377fb1d1ee567264b8e7f3764a1c7f0a94d94f
Airmag7 <airmag@inbox.ru>
2023-01-26 21:57:27 +0300

    work

---
+++ tasks/UniversalSort/ArraySortAdapter.java
@@ -0,0 +1,6 @@
+package tasks.UniversalSort;
+
+public interface ArraySortAdapter {
+    public abstract int compareElements(Object array, int pos1, int pos2);
+    public abstract void swapElements(Object[] array, int pos1, int pos2);
+}
